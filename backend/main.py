eecc1ceba7a629fe1318e3401825a4c557416e22
Mohwit <mohwits@gmail.com>
2024-10-14 19:16:31 +0530

    update backend

--- backend/main.py
+++ backend/main.py
@@ -15,48 +15,48 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome to the P&G CDP Dashboard API"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Welcome to the P&G CDP Dashboard API"}
 
 
-@app.get("/kpis")
+@app.get("/api/kpis")
 async def api_get_kpis():
     return await backend_logic.get_kpis()
 
 
-@app.get("/customer_segments")
+@app.get("/api/customer_segments")
 async def api_get_customer_segments():
     return JSONResponse(content=await backend_logic.get_customer_segments())
 
 
-@app.get("/monthly_revenue")
+@app.get("/api/monthly_revenue")
 async def api_get_monthly_revenue():
     return JSONResponse(content=await backend_logic.get_monthly_revenue())
 
 
-@app.get("/top_customers")
+@app.get("/api/top_customers")
 async def api_get_top_customers():
     return await backend_logic.get_top_customers()
 
 
-@app.get("/product_category_performance")
+@app.get("/api/product_category_performance")
 async def api_get_product_category_performance():
     return JSONResponse(content=await backend_logic.get_product_category_performance())
 
 
-@app.get("/customer_satisfaction")
+@app.get("/api/customer_satisfaction")
 async def api_get_customer_satisfaction():
     return JSONResponse( message = "successfully conected")
     # return JSONResponse(content=await backend_logic.get_customer_satisfaction())
 
 
-@app.get("/churn_risk")
+@app.get("/api/churn_risk")
 async def api_get_churn_risk():
     return JSONResponse(content=await backend_logic.get_churn_risk())
 
 
-@app.get("/rfm_segmentation")
+@app.get("/api/rfm_segmentation")
 async def api_get_rfm_segmentation():
     return JSONResponse(content=await backend_logic.get_rfm_segmentation())
 
